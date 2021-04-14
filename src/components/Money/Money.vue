dc3175531fb0c49cd35f5cffea0e2afce8d5c3a6
Alexey Delendik <delendikalexey@gmail.com>
2021-04-14 17:14:42 +0300

    create money block

---
+++ src/components/Money/Money.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="money">
+    <div class="no-display">{{test}}</div>
+    <div class="money__container">
+      <template v-for="n in array[1]" v-bind:key="n">
+        <img class="money__image"  src="../../images/Money.png" :style="{'left':margin[n-1]}" />
+      </template>
+      
+    </div>
+    <h3 class="money__title">~{{array[0]}} ₽</h3>
+    <p class="money__subtitle">{{subtitle}}  <img src="../../images/info.png" @mouseenter="show = !show" @mouseleave="show = !show" /></p>
+    <p class="money__info" v-if="show">{{info}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Money',
+    props: {
+      array: Array,
+      subtitle: String,
+      info: String,
+    },
+    computed: {
+      mattress : function(){
+        return [this.result*12*3, Math.floor(this.result*12*3/this.oneCoin)]
+      },
+      test : function(){
+        let newArray = []
+        for(let i=0; i<this.array[1]; i++){
+          newArray.push(i*15)
+        }
+        newArray.forEach(item=>this.margin.push(`${item}px`))
+        return newArray
+      }
+    },
+    // render: function(){
+    //   let newArray = []
+    //   for(let i=0; i<this.array[1]; i++){
+    //     newArray.push(i*15)
+    //   }
+    //   newArray.forEach(item=>this.margin.push(`${item}px`))
+    // },
+    data() {
+      return{
+        margin: [],
+        show: false
+      }
+    },
+    watch: {
+      // 'currentValue': function (v) {
+      //   if(v){
+      //     let newArray = []
+      //     for(let i=0; i<this.array[1]; i++){
+      //       newArray.push(i*15)
+      //     }
+      //     newArray.forEach(item=>this.margin.push(`${item}px`))
+      //   }
+      // },
+    },
+    methods: {
+      // test2 : function(){
+      //   let newArray = []
+      //   for(let i=0; i<this.array[1]; i++){
+      //     newArray.push(i*15)
+      //   }
+      //   newArray.forEach(item=>this.margin.push(`${item}px`))
+      //   // return newArray
+      // }
+
+    },
+    // mounted: function(){
+    // this.$nextTick(this.test2)
+    // },
+    // created: function(){
+    //     this.test()
+    // },
+    
+    components:{
+
+    }
+  }
+</script>
+
+<style scoped>
+  @import '../../style/index.css';
+</style>
--- src/components/Result/Result.vue
+++ src/components/Result/Result.vue
@@ -4,14 +4,25 @@
       Вы откладываете {{result}} ₽ в месяц. <br>
       За три года вы бы заработали:
     </h1>
-    <h3>{{mattress}}</h3>
-    <h3>{{deposit}}</h3>
-    <p class="text">Ответьте и узнаете, сколько копят другие.</p>
+    <!-- <h3>{{mattress}}</h3>
+    <div class="money__container">
+      <template v-for="n in first" v-bind:key="n">
+        <img class="money__image"  src="../../images/Money.png" :style="{'left':array}" />
+      </template>
+    </div> -->
+    <div class="result__container">
+      <money :array="mattress" :subtitle="mattressSubtitle" :info="mattressInfo" />
+      <money :array="deposit" :subtitle="depositSubtitle" :info="depositInfo" />
+      <money :array="invest" :subtitle="investSubtitle" :info="investInfo" />
+    </div>
+    <!-- <h3>{{deposit}}</h3>
+    <h3>{{invest}}</h3> -->
 
   </div>
 </template>
 
 <script>
+import Money from '../Money/Money.vue'
   export default {
     name: 'Result',
     watch: {
@@ -26,28 +37,41 @@
     props: {
       result: Number,
       showResult: Boolean,
+      // number: 3,
     },
     computed: {
       mattress : function(){
-        return this.result*12*3
+        return [this.result*12*3, Math.floor(this.result*12*3/this.oneCoin)]
       },
       deposit: function(){
-        let sum = 8888*12
-
+        let sum = this.result*12
         for (let i=0; i<3; i++){
-          
-          sum = sum*(i+1) + sum*0.0698
-          
-          console.log(Math.floor(sum))
+          sum = this.result*12*(i+1) + sum*0.0698
         }
-        // const year = this.result*12
-        return Math.floor(sum)
-        // Math.floor((year+year*0.0698)+year+((year+year*0.0698)+year)*0.0698+year+(((year+year*0.698)+year)*0.0698+year)*0.0698)
-      }
+        return [Math.floor(sum), Math.floor(sum/this.oneCoin)]
+      }, 
+      invest: function(){
+        return [Math.floor(this.result*12*3 + this.result*12*3*0.7121), 10]
+      },
+      oneCoin:function(){
+        return Math.floor(this.invest[0]/10)
+      },
+      // array: function(){
+      //   let x=400+6
+      //   return `${x}px`
+      // },
+
     },
     data() {
       return{
-        // year : result*12,
+
+        mattressSubtitle: "если откладывать под матрас",
+        mattressInfo: "В этом мало смысла — такие накопления «съедает» инфляция",
+        depositSubtitle: "если откладывать на депозит",
+        depositInfo: "Ставки по вкладам различны в разных банках и зависят от многих факторов, в частности, от ключевой ставки Центрального банка РФ¹.",
+        investSubtitle: "если инвестировать в ПИФ «Альфа-Капитала»",
+        investInfo: "Вы становитесь инвестором набора компаний, который определяют профессиональные управляющие. Они решают, когда покупать и продавать ценные бумаги, чтобы обеспечить инвестиционный доход. Купить или продать паи фонда можно в любой момент².",
+        // year : 12,
         // mattress = year*3,
         // deposit = (year+year*0.698)+year+((year+year*0.698)+year)*0.698+year+(((year+year*0.698)+year)*0.698+year)*0.698,
       }
@@ -55,10 +79,24 @@
     methods: {
       // year = result*12,
       // mattress = year*3,
+    },
+    components:{
+        Money
+      // 'money': {
+      //   // 'props':['users'],
+      //   render:function () {
+      //     for(let i=0; i<4; i++) {
+      //       return <img  src="../../images/Money.png" />
+      //     } 
+      //   }
+      // }
     }
   }
 </script>
 
 <style scoped>
+  /* .money__image {
+    --left: {{ year }};
+  } */
   @import '../../style/index.css';
 </style>
